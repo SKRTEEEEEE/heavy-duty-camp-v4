62ca6457869445323e054a316a3a610350507fba
SKRTEEEEEE <adanreh.m@gmail.com>
2025-02-05 01:11:35 +0100

    clase #5; codigo ejemplo p2

--- programs/first-test/src/lib.rs
+++ programs/first-test/src/lib.rs
@@ -8,9 +8,12 @@ pub mod cpi {
     use super::*;
 
     pub fn transferir(ctx: Context<Transferir>, cantidad: u64) -> Result<()> {
-        let de = ctx.accounts.remitente.to_account_info();
+        let de = ctx.accounts.pda_remitente.to_account_info();
         let para = ctx.accounts.recipiente.to_account_info();
         let programa = ctx.accounts.system_program.to_account_info();
+
+        let bump = ctx.bumps.pda_remitente;
+        let semillas_firma: &[&[&[u8]]] = &[&["PDA".as_bytes(), &[bump]]]; //Nuestro semillas necesarias para la firma
 
         let cpi_context = CpiContext::new(
             programa,
@@ -18,7 +21,7 @@ pub mod cpi {
                 from: de,
                 to: para,
             },
-        );
+        ).with_signer(semillas_firma);
 
         transfer(cpi_context, cantidad)?;
         Ok(())
@@ -27,8 +30,8 @@ pub mod cpi {
 
 #[derive(Accounts)]
 pub struct Transferir<'info> {
-    #[account(mut)] // mutable porque se le debita saldo
-    remitente: Signer<'info>,
+    #[account(mut, seeds = ["PDA".as_bytes()], bump)] // mutable porque se le debita saldo, esta sera la PDA
+    pda_remitente: SystemAccount<'info>,
     #[account(mut)] // mutable porque se le aumenta saldo
     recipiente: SystemAccount<'info>,
     system_program: Program<'info, System>,
