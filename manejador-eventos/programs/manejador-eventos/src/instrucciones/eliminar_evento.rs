8fb7418f0c26c998234b909cfb761ba98907b18d
SKRTEEEEEE <adanreh.m@gmail.com>
2025-02-11 12:41:42 +0100

    clase #8; codigo ejemplo - eliminar evento test

--- manejador-eventos/programs/manejador-eventos/src/instrucciones/eliminar_evento.rs
+++ manejador-eventos/programs/manejador-eventos/src/instrucciones/eliminar_evento.rs
@@ -4,11 +4,86 @@ use crate::colecciones::*;
 use crate::utilidades::*;
 
 /*
+Esta instruccion va a consistir en eliminar la cuenta que almacena
+la informacion del evento y las cuentas PDAS relacionadas al mismo
+(boveda del evento, boveda de ganancias, token del evento)
+
+Cuentas:
+- evento: cuenta que almacena la información del evento
+- boveda del evento: cuenta que almacena los tokens obtenidos de los colaboradores
+- boveda de ganancias: cuentq que almacena los tokens obtenidos de la venta de entradas
+- token_evento: token que se entrega a los colaboradores del evento 
+- autoridad: usuario que creo el evento y que será quien puede eliminarlo
+ */
+
+// definimos el contexto
+#[derive(Accounts)]
+pub struct EliminarEvento<'info> {
+    // CUENTAS
+    #[account(
+       mut, 
+       seeds = [ 
+            evento.id.as_ref(),
+            Evento::SEMILLA_EVENTO.as_bytes(),
+            autoridad.key().as_ref(),
+        ],
+        bump = evento.bump_evento,
+        // verificaciones previas a elimimanr la cuenta
+        constraint = evento.entradas_vendidas == 0 @ CodigoError::EventoConSponsors, // no hay sponsors todavía
+        constraint = evento.total_sponsors == 0 @ CodigoError::EventoConSponsors, // no hay sponsors todavía
+        constraint = evento.autoridad == autoridad.key() @ CodigoError::UsuarioNoAutorizado, // el usuario esta autorizado
+        close = autoridad // cierra automaticamente la cuenta evento y devuelve la renta al usuario autoridad
+    )]
+    pub evento: Account<'info, Evento>,
+
+    #[account(
+        mut,
+        seeds = [
+            Evento::SEMILLA_BOVEDA_EVENTO.as_bytes(),
+            evento.key().as_ref(),
+        ],
+        bump = evento.bump_boveda_evento,
+        // verificaciones previas a eliminar la cuenta
+        constraint = boveda_evento.amount == 0 @ CodigoError::BovedaDelEventoNoVacia,  // debe estar vacía
+    )]
+    pub boveda_evento: Account<'info, TokenAccount>,
+
+    #[account(
+        mut,
+        seeds = [
+            Evento::SEMILLA_BOVEDA_GANANCIAS.as_bytes(),
+            evento.key().as_ref(),
+        ],
+        bump = evento.bump_boveda_ganancias,
+        //verificaciones previas a eliminar la cuenta
+        constraint = boveda_ganancias.amount == 0 @ CodigoError::BovedaDeGananciasNoVacia, // debe estar vacía
+    )]
+    pub boveda_ganancias: Account<'info, TokenAccount>,
+
+    #[account(
+        mut,
+        seeds = [
+            Evento::SEMILLA_TOKEN_EVENTO.as_bytes(),
+            evento.key().as_ref(),
+        ],
+        bump = evento.bump_token_evento,
+    )]
+    pub token_evento: Account<'info, Mint>, // vamos a "desactivar" esta cuenta
+
+    #[account(mut)]
+    pub autoridad: Signer<'info>, // usuario que creo el evento
+
+    //PROGRAMAS
+    pub token_program: Program<'info, Token>,
+    pub system_program: Program<'info, System>,
+}
+
+/*
 Esta función es válida para el caso en el que no hay 
 ningun colaborador y no se han vendido tickets, e decir: 
 boveda de ganancias en 0 tokens
 boveda del evento en 0 tokens
-*/
+ */
 pub fn eliminar_evento(ctx: Context<EliminarEvento>) -> Result<()> {
     // La cuenta del Evento que es una PDA es la autoridad sobre todas las cuentas a eliminar
     // por lo que necesitamos sus semillas para firmar
@@ -20,12 +95,11 @@ pub fn eliminar_evento(ctx: Context<EliminarEvento>) -> Result<()> {
     ]];
 
     /*
-        Cerramos las cunetas de las bóvedas:
-        Para esto hacemos un CPI a la instrucción CloseAccount
-        del token program.
+    Cerramos las cunetas de las bóvedas:
+    Para esto hacemos un CPI a la instrucción CloseAccount
+    del token program.
+     */
 
-        PD: La cuenta evento no debemos cerrarla ya que se cierran 'automaticamente' porque se lo hemos indicado
-    */
     // cerramos la boveda del evento
     let cerrar_boveda_evento = CpiContext::new(
         ctx.accounts.token_program.to_account_info(),
@@ -38,18 +112,6 @@ pub fn eliminar_evento(ctx: Context<EliminarEvento>) -> Result<()> {
 
     // llamamos a la CPI
     close_account(cerrar_boveda_evento)?;
-    // cerramos la boveda de ganacias
-    let cerrar_boveda_ganancias = CpiContext::new(
-        ctx.accounts.token_program.to_account_info(),
-        CloseAccount {
-            account: ctx.accounts.boveda_ganancias.to_account_info(), // cuenta a cerrar
-            destination: ctx.accounts.autoridad.to_account_info(), // se devuelve la renta al usuario incializador
-            authority: ctx.accounts.evento.to_account_info(),      // el evento debe autorizar
-        },
-    ).with_signer(semillas_firma); // firma con PDA
-
-    //  llamamos a la CPI
-    close_account(cerrar_boveda_ganancias)?;
 
     // cerramos la boveda de ganacias
     let cerrar_boveda_ganancias = CpiContext::new(
@@ -63,6 +125,7 @@ pub fn eliminar_evento(ctx: Context<EliminarEvento>) -> Result<()> {
 
     //  llamamos a la CPI
     close_account(cerrar_boveda_ganancias)?;
+
     /*
     Una vez eliminadas las cuentas de las bóvedas, lo que sigue es
     eliminar la cuenta Mint del token del evento, sin embargo, esto
@@ -94,77 +157,4 @@ pub fn eliminar_evento(ctx: Context<EliminarEvento>) -> Result<()> {
     // con el Token extensions podriamos cerrar la cuenta Mint al cerrar el evento
 
     Ok(())
-}
-
-/*
-Esta instruccion va a consistir en eliminar la cuenta que almacena
-la informacion del evento y las cuentas PDAS relacionadas al mismo
-(boveda del evento, boveda de ganancias, token del evento)
-
-Cuentas:
-- evento: cuenta que almacena la información del evento
-- boveda del evento: cuenta que almacena los tokens obtenidos de los colaboradores
-- boveda de ganancias: cuentq que almacena los tokens obtenidos de la venta de entradas
-- token_evento: token que se entrega a los colaboradores del evento 
-- autoridad: usuario que creo el evento y que será quien puede eliminarlo
- */
-#[derive(Accounts)]
-pub struct EliminarEvento<'info> {
-    //cuentas
-    #[account(
-        mut,
-        seeds=[
-            evento.id.as_ref(),
-            Evento::SEMILLA_EVENTO.as_bytes(),
-            autoridad.key().as_ref(),
-        ],
-        bump = evento.bump_evento,
-
-        constraint = evento.total_sponsors == 0 @ CodigoError::EventoConSponsors, ,
-        constraint = evento.tokens_vendidos == 0 @ CodigoError::EventoConSponsors, ,
-        constraint = evento.autoridad == autoridad.key() @ CodigoError::UsuarioNoAutorizado,
-        //anotamos la intencion de cerrar la cuenta
-        close = autoridad
-    )]
-    pub evento: Account<'info, Evento>,
-    // tambien cerramos las bovedas
-    #[account(
-        mut,
-        seeds=[
-            Evento::SEMILLA_BOVEDA_EVENTO.as_bytes(),
-            evento.key().as_ref(),
-        ],
-        bump = evento.bump_boveda_evento,
-        constraint = boveda_evento.amount == 0 @ CodigoError::BovedaDelEventoNoVacia,
-    )]
-    pub boveda_evento: Account<'info, TokenAccount>,
-
-    #[account(
-        mut,
-        seeds=[
-            Evento::SEMILLA_BOVEDA_GANANCIAS.as_bytes(),
-            evento.key().as_ref(),
-        ],
-        bump = evento.bump_boveda_ganancias,
-        constraint = boveda_ganancias.amount == 0 @ CodigoError::BovedaDeGananciasNoVacia, 
-        )]
-    pub boveda_ganancias: Account<'info, TokenAccount>,
-
-    //token del evento
-    #[account(
-        mut,
-        seeds = [
-            Evento::SEMILLA_TOKEN_EVENTO.as_bytes(),
-            evento.key().as_ref(),
-        ],
-        bump = evento.bump_token_evento,
-    )]
-    pub token_evento: Account<'info, Mint>, // vamos a "desactivar" esta cuenta ->no existe close, pero en la version con extensiones, de mint_account, no hay close, pero se puede desactivar
-
-    #[account(mut)]
-    pub autoridad: Signer<'info>, // usuario que creo el evento
-
-    //PROGRAMAS
-    pub token_program: Program<'info, Token>,
-    pub system_program: Program<'info, System>,
 }
--- manejador-eventos/tests/manejador-eventos.ts
+++ manejador-eventos/tests/manejador-eventos.ts
@@ -1,9 +1,5 @@
-import * as spl from "@solana/spl-token"
-
-/*
-Version para solpg.io
-*/
-
+import * as spl from "@solana/spl-token";
+//solpg.io version
 describe("Test", () => {
   // declarar las cuentas necesarias
   let autoridad = pg.wallet.keypair;
@@ -11,15 +7,15 @@ describe("Test", () => {
   let tokenAceptado: web3.PublicKey;
 
   let evento: web3.PublicKey;
-  let tokenEvento: web3.PublicKey
-  let bovedaEvento: web3.PublicKey
-  let bovedaGanancias: web3.PublicKey
+  let tokenEvento: web3.PublicKey;
+  let bovedaEvento: web3.PublicKey;
+  let bovedaGanancias: web3.PublicKey;
 
-  let id: string = Date.now().toString()
+  let id: string = Date.now().toString();
 
   //creamos todo lo necesario previamente antes de nuestras instrucciones
-  before(async()=>{
-     // buscamos la PDA del evento
+  before(async () => {
+    // buscamos la PDA del evento
     [evento] = web3.PublicKey.findProgramAddressSync(
       [Buffer.from(id), Buffer.from("evento"), autoridad.publicKey.toBuffer()],
       pg.PROGRAM_ID
@@ -58,16 +54,16 @@ describe("Test", () => {
       autoridad.publicKey, // el freeza authority
       2 // decimales del token
     );
-  })
+  });
   it("Crear un evento", async () => {
-      // Datos básicos del evento
-      const nombre: string = "Mi primer evento";
-      const descripcion = "El mejor evento del mundo!";
-      const precioEntrada = 2.1;
-      const precioToken = 5.0;
+    // Datos básicos del evento
+    const nombre: string = "Mi primer evento";
+    const descripcion = "El mejor evento del mundo!";
+    const precioEntrada = 2.1;
+    const precioToken = 5.0;
 
-      // llamamos a la instruccion del programa
-      const tx = await pg.program.methods
+    // llamamos a la instruccion del programa
+    const tx = await pg.program.methods
       .crearEvento(id, nombre, descripcion, precioEntrada, precioToken)
       .accounts({
         evento: evento,
@@ -79,16 +75,52 @@ describe("Test", () => {
       })
       .rpc();
 
-      //Confirmamos la transaccion
-      await pg.connection.confirmTransaction(tx);
+    //Confirmamos la transaccion
+    await pg.connection.confirmTransaction(tx);
 
-      //Podemos ver la informacion almacenada en la cuenta del evento
-      const infoEvento = await pg.program.account.evento.fetch(evento);
+    //Podemos ver la informacion almacenada en la cuenta del evento
+    const infoEvento = await pg.program.account.evento.fetch(evento);
 
-      console.log("Información del evento: ", infoEvento);
+    console.log("Información del evento: ", infoEvento);
 
-      // con al informacion del evento podemos hacer comprobaciones
-      // comprobamos que el precio del token sea correcto (y esta expresado en la unidad minima del token)
-      assert.equal(infoEvento.precioToken.toNumber(), precioToken * 10 ** 2);
+    // con al informacion del evento podemos hacer comprobaciones
+    // comprobamos que el precio del token sea correcto (y esta expresado en la unidad minima del token)
+    assert.equal(infoEvento.precioToken.toNumber(), precioToken * 10 ** 2);
+  });
+  it("Elimina el evento creado anteriormente", async () => {
+    // comprobaciones previas
+    console.log("eliminando evento...");
+    const bovedaEventoInfo = await spl.getAccount(pg.connection, bovedaEvento);
+    console.log("Saldo de la bóveda del evento:", bovedaEventoInfo.amount);
+
+    const bovedaGananciasInfo = await spl.getAccount(
+      pg.connection,
+      bovedaGanancias
+    );
+    console.log("Saldo de la bóveda de ganancias:", bovedaGananciasInfo.amount);
+
+    // llamamo a la instrucción eliminar
+    const tx = await pg.program.methods
+      .eliminarEvento()
+      // enviamos las cuentas asociadas a la instrucción
+      .accounts({
+        evento: evento,
+        bovedaEvento: bovedaEvento,
+        bovedaGanancias: bovedaGanancias,
+        tokenEvento: tokenEvento,
+        autoridad: autoridad.publicKey,
+      })
+      // firma la autoridad creadora del evento
+      .signers([autoridad])
+      // enviamos a la red
+      .rpc();
+
+    await pg.connection.confirmTransaction(tx);
+
+    //Podemos ver la informacion almacenada en la cuenta del evento
+    // en este caso debe ser null porque no debe existir
+    const infoEvento = await pg.program.account.evento.fetchNullable(evento); // Esta funcion .fetchNullable(), nos permite esperar un null, asi no nos dara un error, al utilizar -.fetch()
+
+    console.log("Información del evento: ", infoEvento);
   });
 });
